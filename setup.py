31264f61cb3ee798cf29994b75e551a133232f5b
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2018-11-11 18:55:12 -0600

    package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AutoSite",
+    version="1.0.0",
+    author="dotcomboom",
+    author_email="dotcomboom@protonmail.com",
+    description="Keep all your website's pages under one template",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dotcomboom/AutoSite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup :: HTML"
+    ],
+)
